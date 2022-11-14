9bf7ef33b022e4c800d12caccbcfa9be51f03091
Ady Ngom <ady.touba.ngom@gmail.com>
2022-11-14 00:39:11 -0500

    launch push

---
+++ src/interfaces/interfaces.ts
@@ -0,0 +1,31 @@
+export interface Speaker {
+    id: string;
+    name: string;
+}
+
+export interface SessionSpeaker extends Speaker {
+    firstName: string;
+    lastName: string;
+    fullName: string;
+    tagLine: string;
+    profilePicture: string;
+    isTopSpeaker: boolean;
+}
+
+export interface Session {
+    questionAnswers?: any[];
+    id: string;
+    title: string;
+    description: string;
+    startsAt?: any;
+    endsAt?: any;
+    isServiceSession: boolean;
+    isPlenumSession: boolean;
+    speakers: Speaker[];
+    categories: any[];
+    roomId?: any;
+    room?: any;
+    liveUrl?: any;
+    recordingUrl?: any;
+}
+
